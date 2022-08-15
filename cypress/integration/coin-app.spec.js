e41b0ee51e8c5ed10242942641845504537ccd82
Olga Yefanova <olanef9@gmail.com>
2022-08-15 14:28:37 +0500

    Update coin-app.spec.js
    deleted comments

--- cypress/integration/coin-app.spec.js
+++ cypress/integration/coin-app.spec.js
@@ -13,9 +13,6 @@ describe('Онлайн-банк Coin', () => {
     // enter valid login and password
     cy.get('#login').type('developer{enter}');
     cy.get('#password').type('skillbox{enter}');
-
-    // cy.get('#login').click();
-    // cy.get('#password').click();
     cy.get('body').click();
     cy.contains('button', 'Войти').click();
 
@@ -23,53 +20,6 @@ describe('Онлайн-банк Coin', () => {
     cy.location('pathname').should('equal', '/index.html/accounts');
     cy.contains('Ваши счета').should('be.visible');
   });
-
-  // it('Отсортировать счета по номеру', () => {
-  //   cy.get('.select-selected').click();
-  //   cy.get('.select-items div').first().click();
-  //   cy.get('h3').then(($els) => {
-  //     const arr = Array.from($els, (el) => el.innerText);
-  //     for (let i = 0; i < arr.length; i++) {
-  //       if (arr[i] <= arr[i + 1] && arr[i] < arr[arr.length - 1]) {
-  //         return;
-  //       } else {
-  //         throw new Error('Сортировка выполнена неверно');
-  //       }
-  //     }
-  //   });
-  // });
-
-  // it('Отсортировать счета по балансу', () => {
-  //   cy.get('.select-selected').click();
-  //   cy.get('.select-items div').eq(1).click();
-  //   cy.get('.main__card_balance').then(($els) => {
-  //     const arr = Array.from($els, (el) => el.innerText.slice(0, -1));
-  //     for (let i = 0; i < arr.length - 1; i++) {
-  //       if (arr[i] < arr[i + 1]) {
-  //         //expect(10).to.be.lessThan(12);
-  //         //alert(arr[i] < arr[i + 1]);
-  //         return;
-  //       } else {
-  //         throw new Error('Сортировка выполнена неверно');
-  //       }
-  //     }
-  //   });
-  // });
-
-  // it('Отсортировать счета по последней транзакции', () => {
-  //   cy.get('.select-selected').click();
-  //   cy.get('.select-items div').eq(2).click();
-  //   cy.get('.main__trans-date').then(($els) => {
-  //     const arr = Array.from($els, (el) => el.dataset.date);
-  //     for (let i = 0; i < arr.length; i++) {
-  //       if (arr[i] < arr[i + 1]) {
-  //         return;
-  //       } else {
-  //         throw new Error('Сортировка выполнена неверно');
-  //       }
-  //     }
-  //   });
-  // });
 
   it('Открыть подробности счета и вернуться на страницу со всеми счетами', () => {
     cy.get('.main__block .main__card:first-child .main__card_number').then(
